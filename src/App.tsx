dc80c26e891d28946062584ecd290df49c58a818
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-09-18 19:12:53 +0500

    Added Basic Structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import LaunchedMissionsss from './components/LaunchedMissions/index'
-import Missions from './components/MissionDetail/index'
 function App() {
   return (
     <div className="App">
       <LaunchedMissionsss/>
-      <Missions/>
     </div>
   );
 }
