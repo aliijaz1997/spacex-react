dc4fd297f6ae76c94a34b43de49ee98b9edd7cba
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-09-18 17:07:38 +0500

    install material ui

--- src/components/MissionDetail/Detail.tsx
+++ src/components/MissionDetail/Detail.tsx
@@ -6,17 +6,12 @@ interface Props {
 const DetailOfMission : React.FC<Props> = ({data}) => {
     return (
       <div className="details-container">
-      <div className="details">
-          <div className="big-img">
-           
-          </div>
-          <div className="box">
-              <div className="row">
+     
+          
                   <h2>{data?.launch?.mission_name}</h2>
-              </div>
+            
               <span>Launched in {data?.launch?.launch_year} from {data?.launch?.launch_site?.site_name}</span>
-               </div>
-      </div>
+   
   </div>
 
     )
